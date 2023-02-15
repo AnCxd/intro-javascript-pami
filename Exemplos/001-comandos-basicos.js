ed904f4cb23e172adfcf8b06fc275fafbe2c4c3a
Lucas-Costa-Xavier <124743196+Lucas-Costa-Xavier@users.noreply.github.com>
2023-02-14 21:20:05 -0300

    Teste js// node

---
+++ Exemplos/001-comandos-basicos.js
@@ -0,0 +1,32 @@
+console.log('hello world');
+/*
+
+comentario de mais de uma linha
+
+*/
+let nome = 'Ewerton';
+let idade =35;
+let altura = 1.76;
+let trabalha = true;
+let corFavorita = 'laranja';
+let cores_favoritas = ['laranja','roxo','verde','azul']
+let endereco ={
+    'logadouro' : 'Rua',
+    'nome_log'  : 'Brasil',
+    'n'         : '105 A',
+    'bairro'    : 'centro'
+};
+let covid;
+let dengue = null;
+const exfunc = () => {};
+
+console.log('Variavel nome = ' +typeof(nome));
+console.log('Variavel idade = ' +typeof(idade));
+console.log('Variavel altura = ' +typeof(altura));
+console.log('Variavel trabalha = ' +typeof(trabalha));
+console.log('Variavel corFavorita = ' +typeof(corFavorita));
+console.log('Variavel cores_favoritas = ' +typeof(cores_favoritas));
+console.log('Variavel endereco = ' +typeof(endereco));
+console.log('Variavel covid = ' +typeof(covid));
+console.log('Variavel dengue = ' +typeof(dengue));
+console.log('variavel exfunc = '+ typeof (exfunc));
