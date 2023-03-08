91808e65e1cc33f6c56d6ccc977d3b4376f390fd
Lucas-Costa-Xavier <124743196+Lucas-Costa-Xavier@users.noreply.github.com>
2023-03-07 22:19:50 -0300

    att

--- Exemplos/007-atividade-repeticao.js
+++ Exemplos/007-atividade-repeticao.js
@@ -4,31 +4,27 @@
     SE O VALOR INICIAL FOR MAIOR QUE O FINAL EXIBA A CONTAGEM DECRESCENTE 
 */
 
+// recebe o valor inicial e final do usuário
 let entrada = require('prompt-sync')();
 
-console.log('Atividade Repetição')
-let valor;
-valor = entrada('Insira o valor inicial: ');
-let n1 = parseint(valor);
-valor = entrada('Insira o valor final: ');
-let n2 = parseint(valor);
+let v1 = entrada ('Digite o valor inicial: ');
+let v2 = entrada ('Digite o valor final: ');
 
-if (n1 < n2){
+let valorinicial = parseInt(v1);
+let valorfinal = parseInt(v2);
 
-    for (let cont = n1; cont <= n2; cont++){
-    console.log(cont);
+// verifica se o valor inicial é menor que o final
+if (valorinicial < valorfinal){
+    // contador crescente
+    for (let i = valorinicial; i <= valorfinal; i++) {
+        console.log(i);
+    }
+} else if (valorinicial > valorfinal) {
+    //contador decrescente
+    for (let i = valorinicial; i >= valorfinal; i --){
+        console.log(i);
+    }
+}else {
+    //valores iguais
+    console.log("Os valores são iguais.");
 }
-
-}
-
-if (n1 > n2){
-    let cont = n1;
-    do {
-        console.log(cont);
-        cont--;
-    
-    } while(cont >= n2);
-}
-console.log('');
-console.log('Fim Do Programa');
-entrada();
